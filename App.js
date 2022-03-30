d27840d219dc1d7e8d9cddddedb263150eb0e868
CalvinC44 <calel44jke@gmail.com>
2022-03-30 11:48:14 +0200

    maj

--- App.js
+++ App.js
@@ -1,19 +1,75 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  ScrollView
+} from "react-native";
 import SerieManga from "./components/SerieManga";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.mangaWrapper}>
         <Text style={styles.sectionTitle}>Séries de manga</Text>
-        <View style={styles.items}>
+        <ScrollView style={styles.items}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Serie");
+            }}
+          >
+            <SerieManga />
+          </TouchableOpacity>
           <SerieManga />
           <SerieManga />
-          <SerieManga />
-        </View>
+        </ScrollView>
       </View>
     </View>
+  );
+}
+
+function SerieScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: "red", padding: 20 }}
+        onPress={() => {
+          navigation.navigate("Manga");
+        }}
+      ></TouchableOpacity>
+    </View>
+  );
+}
+
+function MangaScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Manga screen</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: "red", padding: 20 }}
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      ></TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Serie" component={SerieScreen} />
+        <Stack.Screen name="Manga" component={MangaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- components/SerieManga.js
+++ components/SerieManga.js
@@ -1,5 +1,7 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+import { View, Text, StyleSheet, Image, Button } from "react-native";
 
 const SerieManga = (props) => {
   return (
